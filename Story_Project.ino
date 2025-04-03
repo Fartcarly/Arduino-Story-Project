45b3df93caccf72514fbd91247eb42a9f3f2dbc0
Carly Mills <164084060+Fartcarly@users.noreply.github.com>
2025-04-03 00:00:46 -0500

    Add files via upload

---
+++ Story_Project.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+ //Hi! this is Carly's comment
+ //Nick change this comment here to say something else
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
