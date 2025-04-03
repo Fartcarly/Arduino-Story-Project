58a3cdea1ecfbf53d0417f72f36494b40314eaca
Carly Mills <164084060+Fartcarly@users.noreply.github.com>
2025-04-03 00:03:19 -0500

    Update Story_Project.ino

--- Story_Project.ino
+++ Story_Project.ino
@@ -1,6 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
  //Hi! this is Carly's comment
+  //writing things on the Carly Branch...la la la
  //Nick change this comment here to say something else
 }
 
