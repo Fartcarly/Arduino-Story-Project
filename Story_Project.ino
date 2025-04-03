c71cb4a04b0edc2d399794ea87279a597946560d
Carly Mills <164084060+Fartcarly@users.noreply.github.com>
2025-04-03 00:05:57 -0500

    Update Story_Project.ino

--- Story_Project.ino
+++ Story_Project.ino
@@ -2,6 +2,7 @@ void setup() {
   // put your setup code here, to run once:
  //Hi! this is Carly's comment
   //writing things on the Carly Branch...la la la
+  //Okay so I made a pull request and then I committed it and merged it with the main branch, then I deleted the Carly branch. Now I'm editing the main code.
  //Nick change this comment here to say something else
 }
 
