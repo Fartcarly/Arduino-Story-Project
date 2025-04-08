2392603b8ca0ec849a4511d4ffccd567655e1d47
Carly Mills <164084060+Fartcarly@users.noreply.github.com>
2025-04-08 18:55:58 -0500

    Update Story_Project.ino
    code for scene 2 only, setup for all scenes

--- Story_Project.ino
+++ Story_Project.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>
+#include <Servo.h> //servo library
 
 //PIN INITIALIZATION (1-13)
 
@@ -6,8 +6,9 @@ const int ledPin1 = 13; //luigis idea lightbulb scene 2
 const int newsLed1 = 12; //news lights scene 3
 const int newsLed2 = 11; 
 const int switchPin1Light = 10; //set up light switch pin with alligator clips
-const int switchPin2Slider = 9; //slider twitter switch for news scene 3
-const int switchPin3Bullet = 8; //bullet switch scene 4
+  int switchState1;  //tracks the state of the switch
+const int switchPin2Bullet = 9; //bullet switch scene 3
+const int switchPin3Slider = 8; //slider twitter switch for news scene 4
 const int switchPin4Stand =  7;//luigi stand switch scene 5
 
 Servo armServo; //creates servo variable for moving luigi's arm
@@ -17,21 +18,76 @@ Servo worldServo; // moves the people of the world holding hands
 
 void setup() {
   // put your setup code here, to run once:
+    Serial.begin(9600); // Start serial communication
+
+//-------SCENE 2---------//
 armServo.attach(6); //attaches arm servo to pin 8
 pinMode(switchPin1Light, INPUT);
+pinMode(ledPin1, OUTPUT); 
 
+//-------SCENE 3---------//
 runServo.attach(5);
-pinMode(switchPin3Bullet, INPUT);
+pinMode(switchPin2Bullet, INPUT);
 
+//-------SCENE 4---------//
+pinMode(switchPin3Slider, INPUT);
+
+//-------SCENE 5---------//
 worldServo.attach(4);
 pinMode(switchPin4Stand, INPUT);
 }
 
+void moveMotor1() {  //moves arm Servo
+  int pos = 0;    // variable to store the servo position
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    armServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    armServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+}
+void moveMotor2() {  //moves run Servo
+  int pos = 0;    // variable to store the servo position
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    runServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    runServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void moveMotor3() {  //moves world Servo
+  int pos = 0;    // variable to store the servo position
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    worldServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    worldServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-if (digitalRead(switchPin1)==HIGH){
-  armservo.write(180); //servo angle
+  switchState1 = digitalRead(switchPin1Light);
+if (digitalRead(switchPin1Light)==HIGH){
+  digitalWrite(ledPin1, LOW); //turn light on
+  moveMotor1();
+  Serial.println("Button Pressed"); // Print message when button is pressed
+
 }else{
-  armservo.write(0);
+  if (digitalRead(switchPin1Light)==LOW){
+    digitalWrite(ledPin1, HIGH); //turn light off
+    Serial.println("Button Released"); // Print message when button is not pressed
+  }
+  delay(100);
   }
 }
